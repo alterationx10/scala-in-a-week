c15396fad0ccb30f9c07cc5ededcc3555494ce70
Mark Rudolph <mark@k8ty.app>
2020-09-09 22:07:54 -0400

    let there be apps

--- typescript/src/server.ts
+++ typescript/src/server.ts
@@ -1,2 +1,13 @@
+import express from "express";
+import { config } from "./config/config";
+import { healthRoutes } from './controllers/health/routes'
+import { rootRoutes } from './controllers/root/routes'
 
-console.log('Hello, Typescript!');
+const app = express();
+
+healthRoutes(app);
+rootRoutes(app);
+
+app.listen(config.EXPRESS_PORT, () => {
+    console.log(`Server listenng at http://localhost:${config.EXPRESS_PORT}`);
+});
