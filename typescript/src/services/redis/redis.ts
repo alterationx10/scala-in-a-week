9a8420094c35c9c39016480c9e5d3382df59f6b3
Mark Rudolph <mark@k8ty.app>
2020-09-11 23:37:02 -0400

    Move redis and minio to a service

---
+++ typescript/src/services/redis/redis.ts
@@ -0,0 +1,28 @@
+import * as redis from 'redis';
+import { RedisClient } from 'redis';
+import { config } from '../../config/config';
+
+export const redisClient = redis.createClient({
+    host: config.REDIS_HOST
+});
+
+export class RedisService {
+    redisClient: RedisClient = redisClient;
+    RedisService(){};
+
+    hgetInt(key: string, field: string, _default: number = 0): Promise<number>  {
+        return new Promise(function(resolve, _) {
+            redisClient.hget(key, field, (_, str) => {
+                if (str) {
+                    try {
+                        resolve(parseInt(str));
+                    } catch (e) {
+                        // do nothing
+                    }
+                }
+                resolve(_default);
+            
+            });
+        });        
+    }
+}
