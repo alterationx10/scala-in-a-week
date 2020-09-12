a16ce2b6a1e75d53843334653e2ce648daaa83a3
Mark Rudolph <mark@k8ty.app>
2020-09-11 23:38:04 -0400

    set up stats

---
+++ typescript/src/controllers/api/stats/routes.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { BucketItem } from "minio";
+import { MinioService } from "../../../services/minio/minio";
+import { redisClient, RedisService } from "../../../services/redis/redis";
+
+const minioService = new MinioService();
+const redisService = new RedisService();
+
+export function apiStats(app: Router) {
+
+    app.get('/api/stats', (req, res) => {
+        minioService.bucketList('photos')
+        .then( async (data: BucketItem[]) => {            
+            const result: any[] = [];
+            for (const thing of data) {
+                const views = await redisService.hgetInt(thing.name, 'views');
+                const likes = await redisService.hgetInt(thing.name, 'likes');;
+                result.push({id: thing.name, views, likes});
+            }
+            res.send(result);
+        })
+        .catch((error) => res.status(500).send(error));
+    });
+
+    app.get('/api/stats/:id', async (req, res) => {
+        const id = req.params['id'];
+        const views = await redisService.hgetInt(id, 'views');
+        const likes = await redisService.hgetInt(id, 'likes');;
+        res.send({id, views, likes});
+    });
+
+    app.post('api/like/:id', (req, res) => {
+        try  {
+            const id = req.params['id'];
+            redisClient.hincrby(id, 'likes', 1);
+            res.status(200).send();
+        } catch (e) {
+            res.status(500).send(e);
+        }
+    });
+
+}
--- typescript/src/server.ts
+++ typescript/src/server.ts
@@ -3,12 +3,14 @@ import { config } from "./config/config";
 import { healthRoutes } from './controllers/health/routes'
 import { rootRoutes } from './controllers/root/routes'
 import { apiImages } from "./controllers/api/images/routes";
+import { apiStats } from "./controllers/api/stats/routes";
 
 const app = express();
 
 healthRoutes(app);
 rootRoutes(app);
 apiImages(app);
+apiStats(app);
 
 app.listen(config.EXPRESS_PORT, () => {
     console.log(`Server listenng at http://localhost:${config.EXPRESS_PORT}`);
